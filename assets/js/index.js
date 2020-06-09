849247e4e360f9ec1725b22ab4441f67ec561a97
ShunyaSuzuki <anyushu2017@gmail.com>
2020-06-09 17:16:07 +0900

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,28 +5,28 @@ import '../sass/app.scss';
 import 'bootstrap';
 
 $(function ($) {
-  let w = $(window).width();
-  let md = 991.98;
+  // let w = $(window).width();
+  // let md = 991.98;
 
-  if (w <= md) {
-    // 画像src切り替え
-    $('.img-switch').each(function () {
-      $(this).attr('src', $(this).attr('src').replace('_pc', '_sp'));
-    });
-  }
+  // if (w <= md) {
+  //   // 画像src切り替え
+  //   $('.img-switch').each(function () {
+  //     $(this).attr('src', $(this).attr('src').replace('_pc', '_sp'));
+  //   });
+  // }
 
-  // スムーススクロール
-  $('a[href^="#"]').on('click', function () {
-    var speed = 500;
-    var href = $(this).attr('href');
-    var target = $(href == '#' || href == '' ? 'html' : href);
-    var position = target.offset().top - 100;
-    $('html, body').animate({ scrollTop: position }, speed, 'swing');
-    return false;
-  });
+  // // スムーススクロール
+  // $('a[href^="#"]').on('click', function () {
+  //   var speed = 500;
+  //   var href = $(this).attr('href');
+  //   var target = $(href == '#' || href == '' ? 'html' : href);
+  //   var position = target.offset().top - 100;
+  //   $('html, body').animate({ scrollTop: position }, speed, 'swing');
+  //   return false;
+  // });
 
-  // スクロールイベント
-  $(window).on('scroll', function () {
-    let s = $(window).scrollTop();
-  });
+  // // スクロールイベント
+  // $(window).on('scroll', function () {
+  //   let s = $(window).scrollTop();
+  // });
 });
