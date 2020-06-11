7e0ac372cd4ce41fff0ea4f9da34e4c971396ee1
ShunyaSuzuki <anyushu2017@gmail.com>
2020-06-11 12:46:41 +0900

    update

--- front-page.php
+++ front-page.php
@@ -3,70 +3,127 @@ $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); ?>
 <section class="mv">
-  <div class="container">
-    <h2>
-      <span>
-        <img src="<?php echo $wp_url; ?>/dist/images/mv_icon_01.png" alt="アイコン" srcset="<?php echo $wp_url; ?>/dist/images/mv_icon_01.png 1x, <?php echo $wp_url; ?>/dist/images/mv_icon_01@2x.png 2x">
-        近くのテイクアウトの<br>名店を探す
-        <img src="<?php echo $wp_url; ?>/dist/images/mv_icon_02.png" alt="アイコン" srcset="<?php echo $wp_url; ?>/dist/images/mv_icon_02.png 1x, <?php echo $wp_url; ?>/dist/images/mv_icon_02@2x.png 2x">
-      </span>
-    </h2>
-    <div class="text-center mt-4">
-      <button class="btn btn-primary text-white"><i class="fas fa-search mr-2"></i>近くのテイクアウト対応店を探す</button>
-    </div>
-  </div>
+<div class="container">
+<h2>
+<span>
+<img src="<?php echo $wp_url; ?>/dist/images/mv_icon_01.png" alt="アイコン" srcset="<?php echo $wp_url; ?>/dist/images/mv_icon_01.png 1x, <?php echo $wp_url; ?>/dist/images/mv_icon_01@2x.png 2x">
+近くのテイクアウトの<br>名店を探す
+<img src="<?php echo $wp_url; ?>/dist/images/mv_icon_02.png" alt="アイコン" srcset="<?php echo $wp_url; ?>/dist/images/mv_icon_02.png 1x, <?php echo $wp_url; ?>/dist/images/mv_icon_02@2x.png 2x">
+</span>
+</h2>
+<div class="text-center mt-4">
+<button class="btn btn-primary text-white"><i class="fas fa-search mr-2"></i>近くのテイクアウト対応店を探す</button>
+</div>
+</div>
 </section>
 
 <section class="search">
-  <div class="container">
-    <div class="search__area">
-      <h3>よく見られているエリア</h3>
-      <ul>
-        <li><a href="">東京</a></li>
-        <li><a href="">東京</a></li>
-        <li><a href="">東京</a></li>
-        <li><a href="">東京</a></li>
-      </ul>
-    </div>
-  </div>
-  <div class="search__genre bg-light">
-    <h3><div class="container">ジャンルから探す</div></h3>
-    <div class="search__genre-list">
-      <ul>
-        <li>
-          <img src="<?php echo $wp_url; ?>/dist/images/genre_sushi.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_sushi.png 1x, <?php echo $wp_url; ?>/dist/images/genre_sushi@2x.png 2x">
-          <span>寿司</span>
-        </li>
-        <li>
-          <img src="<?php echo $wp_url; ?>/dist/images/genre_sakana.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_sakana.png 1x, <?php echo $wp_url; ?>/dist/images/genre_sakana@2x.png 2x">
-          <span>魚料理</span>
-        </li>
-        <li>
-          <img src="<?php echo $wp_url; ?>/dist/images/genre_washoku.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_washoku.png 1x, <?php echo $wp_url; ?>/dist/images/genre_washoku@2x.png 2x">
-          <span>和食</span>
-        </li>
-        <li>
-          <img src="<?php echo $wp_url; ?>/dist/images/genre_okonomi.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_okonomi.png 1x, <?php echo $wp_url; ?>/dist/images/genre_okonomi@2x.png 2x">
-          <span>お好み焼き</span>
-        </li>
-        <li>
-          <img src="<?php echo $wp_url; ?>/dist/images/genre_chuka.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_chuka.png 1x, <?php echo $wp_url; ?>/dist/images/genre_chuka@2x.png 2x">
-          <span>中華</span>
-        </li>
-        <li>
-          <img src="<?php echo $wp_url; ?>/dist/images/genre_italy.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_italy.png 1x, <?php echo $wp_url; ?>/dist/images/genre_italy@2x.png 2x">
-          <span>イタリアン</span>
-        </li>
-        <li>
-          <img src="<?php echo $wp_url; ?>/dist/images/genre_niku.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_niku.png 1x, <?php echo $wp_url; ?>/dist/images/genre_niku@2x.png 2x">
-          <span>焼肉</span>
-        </li>
-        <li>
-          <img src="<?php echo $wp_url; ?>/dist/images/genre_hum.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_hum.png 1x, <?php echo $wp_url; ?>/dist/images/genre_hum@2x.png 2x">
-          <span>ファストフード</span>
-        </li>
-      </ul>
-    </div>
-  </div>
+<div class="container">
+<div class="search__area">
+<h3>よく見られているエリア</h3>
+<ul>
+<li><a href="">東京</a></li>
+<li><a href="">東京</a></li>
+<li><a href="">東京</a></li>
+<li><a href="">東京</a></li>
+</ul>
+</div>
+</div>
+<div class="search__genre bg-light">
+<h3><div class="container">ジャンルから探す</div></h3>
+<div class="search__genre-list">
+<ul>
+<li>
+<img src="<?php echo $wp_url; ?>/dist/images/genre_sushi.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_sushi.png 1x, <?php echo $wp_url; ?>/dist/images/genre_sushi@2x.png 2x">
+<span>寿司</span>
+</li>
+<li>
+<img src="<?php echo $wp_url; ?>/dist/images/genre_sakana.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_sakana.png 1x, <?php echo $wp_url; ?>/dist/images/genre_sakana@2x.png 2x">
+<span>魚料理</span>
+</li>
+<li>
+<img src="<?php echo $wp_url; ?>/dist/images/genre_washoku.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_washoku.png 1x, <?php echo $wp_url; ?>/dist/images/genre_washoku@2x.png 2x">
+<span>和食</span>
+</li>
+<li>
+<img src="<?php echo $wp_url; ?>/dist/images/genre_okonomi.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_okonomi.png 1x, <?php echo $wp_url; ?>/dist/images/genre_okonomi@2x.png 2x">
+<span>お好み焼き</span>
+</li>
+<li>
+<img src="<?php echo $wp_url; ?>/dist/images/genre_chuka.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_chuka.png 1x, <?php echo $wp_url; ?>/dist/images/genre_chuka@2x.png 2x">
+<span>中華</span>
+</li>
+<li>
+<img src="<?php echo $wp_url; ?>/dist/images/genre_italy.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_italy.png 1x, <?php echo $wp_url; ?>/dist/images/genre_italy@2x.png 2x">
+<span>イタリアン</span>
+</li>
+<li>
+<img src="<?php echo $wp_url; ?>/dist/images/genre_niku.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_niku.png 1x, <?php echo $wp_url; ?>/dist/images/genre_niku@2x.png 2x">
+<span>焼肉</span>
+</li>
+<li>
+<img src="<?php echo $wp_url; ?>/dist/images/genre_hum.png" alt="料理のジャンル" srcset="<?php echo $wp_url; ?>/dist/images/genre_hum.png 1x, <?php echo $wp_url; ?>/dist/images/genre_hum@2x.png 2x">
+<span>ファストフード</span>
+</li>
+</ul>
+</div>
+</div>
+</section>
+
+<section class="sec sec-border">
+<div class="container">
+<h2 class="ttl-h2">いま話題のお店</h2>
+<div class="shop-buzz">
+<div class="shop-buzz__list">
+<?php for ($i=1; $i <= 5; $i++): ?>
+<div class="shop-buzz__list-inner">
+<img src="<?php echo $wp_url; ?>/dist/images/topic_sample.png" alt="" srcset="<?php echo $wp_url; ?>/dist/images/topic_sample.png 1x, <?php echo $wp_url; ?>/dist/images/topic_sample@2x.png 2x">
+<h3>店舗名店舗名店舗名</h3>
+<div class="shop-buzz__list-inner-tag">
+<span class="shop-buzz__list-inner-tag-map">京都</span>
+<span class="shop-buzz__list-inner-tag-genre">ジャンル</span>
+</div>
+</div>
+<?php endfor; ?>
+</div>
+</div>
+</div>
+</section>
+
+<section class="sec sec-border">
+<div class="container">
+<h2 class="ttl-h2">おすすめ特集</h2>
+<div class="featured">
+<?php for ($i=1; $i <= 5; $i++): ?>
+<a class="featured-article" href="">
+<?php if($i === 1): ?>
+<span class="featured-article-new">New!</span>
+<?php endif; ?>
+<img src="<?php echo $wp_url; ?>/dist/images/topic_sample.png" alt="" srcset="<?php echo $wp_url; ?>/dist/images/topic_sample.png 1x, <?php echo $wp_url; ?>/dist/images/topic_sample@2x.png 2x">
+<h3>宅飲みに最適なおつまみテイクアウト特集</h3>
+</a>
+<?php endfor; ?>
+</div>
+</div>
+</section>
+
+<section class="sec sec-border">
+<div class="container">
+<h2 class="ttl-h2">いま話題のお店</h2>
+<div class="menu__ranking">
+<?php for ($i=1; $i <= 5; $i++): ?>
+<a class="menu__ranking__inner" href="">
+<div class="menu__ranking__inner__thumbnail">
+<span class="menu__ranking__inner__thumbnail-no"><?php echo $i; ?></span>
+<img src="<?php echo $wp_url; ?>/dist/images/topic_sample.png" alt="" srcset="<?php echo $wp_url; ?>/dist/images/topic_sample.png 1x, <?php echo $wp_url; ?>/dist/images/topic_sample@2x.png 2x">
+<span class="menu__ranking__inner__thumbnail-area">京都</span>
+</div>
+<p class="menu__ranking__inner-name">バジルソースのパスタ</p>
+<p class="menu__ranking__inner-shop">TakeCafe</p>
+<p class="menu__ranking__inner-price">¥<span>980</span>[税込]</p>
+</a>
+<?php endfor; ?>
+</div>
+</div>
 </section>
 <?php get_footer();
