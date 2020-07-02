650268ad009e146f1072689ff100e81fbd9d152c
ShunyaSuzuki <anyushu2017@gmail.com>
2020-07-02 18:10:34 +0900

    udpate

--- header.php
+++ header.php
@@ -19,12 +19,32 @@ $wp_url = get_template_directory_uri(); ?>
 <?php endif; ?>
 </head>
 <body>
-<header class="shadow-sm">
-<nav class="navbar navbar-expand-md justify-content-start align-items-center">
+<header class="header shadow-sm">
+<nav class="navbar navbar-expand-xlg justify-content-start align-items-center">
 <a class="navbar-brand p-0 mb-1" href="<?php echo $home; ?>">
 <img src="<?php echo $wp_url; ?>/dist/images/logo.png" alt="<?php bloginfo('name'); ?>" srcset="<?php echo $wp_url; ?>/dist/images/logo.png 1x, <?php echo $wp_url; ?>/dist/images/logo@2x.png 2x">
 </a>
 <h1 class="navbar-brand m-0 text-white bg-primary py-1 px-3">お店検索</h1>
+
+<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<i class="fas fa-bars"></i>
+</button>
+
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav mr-auto mt-3">
+<li class="nav-item border-top">
+<a class="nav-link text-body font-weight-bold d-block py-3" href="<?php echo $home; ?>">トップページ</a>
+</li>
+<li class="nav-item border-top">
+<a class="nav-link text-body font-weight-bold d-block py-3" href="https://system.take-eats.jp" target="_blank">当サイトへの掲載について</a>
+</li>
+<li class="nav-item border-top">
+<a class="nav-link text-body font-weight-bold d-block py-3" href="<?php echo $home; ?>/special/">特集</a>
+</li>
+</ul>
+</div>
+
 </nav>
 </header>
 <main>
