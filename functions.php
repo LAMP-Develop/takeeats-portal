c3728c3c93601d6d6c5b2fe6dd3033a30b1eb3c2
ShunyaSuzuki <shunya_suzuki@ShunyaSuzukinoMacBook-Air.local>
2020-08-28 16:46:37 +0900

    update

--- functions.php
+++ functions.php
@@ -95,6 +95,13 @@ add_action('after_setup_theme', 'gutenberg_support_setup');
 function twpp_enqueue_styles()
 {
     wp_enqueue_style(
+        'default-style',
+        get_template_directory_uri().'/style.css',
+        [],
+        '1.0.0',
+        'all'
+    );
+    wp_enqueue_style(
         'main-style',
         get_template_directory_uri().'/dist/css/style.css',
         [],
